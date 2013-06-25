38cc4391ddc7255c85997f763789993d15532374
yumin <chinawym@gmail.com>
2013-06-25 17:14:45 +0800

    精简

--- src/me/yumin/android/common/etc/CommonConstant.java
+++ src/me/yumin/android/common/etc/CommonConstant.java
@@ -12,44 +12,11 @@ public final class CommonConstant {
 	// file
 	public static final String F_CONFIG_PROPERTIES = "properties/config.properties";
 	// key:
-	public static final String K_APP_KEY = "appKey";
-	public static final String K_BUYER_NICK = "buyerNick";
-	public static final String K_CHECK_CODE = "checkCode";
-	public static final String K_CHECK_CODE_ID = "checkCodeId";
-	public static final String K_CHECK_CODE_URL = "checkCodeUrl";
-	public static final String K_CODE_LEFT_NUM = "codeLeftNum";
-	public static final String K_ECODE = "ecode";
-	public static final String K_ETICKET_TOKEN = "eticketToken";
-	public static final String K_DATA = "data";
-	public static final String K_IMEI = "imei";
-	public static final String K_IMSI = "imsi";
-	public static final String K_ITEM_PIC_URL = "itemPicUrl";
-	public static final String K_ITEM_TITLE = "itemTitle";
-	public static final String K_KEY = "key";
 	public static final String K_LOG = "log";
-	public static final String K_LOGIN_TIME = "logintime";
 	public static final String K_MODE = "mode";
-	public static final String K_NICK = "nick";
-	public static final String K_PASSWORD = "password";
-	public static final String K_PUB_KEY = "pubKey";
-	public static final String K_RET = "ret";
-	public static final String K_SELLER_NICK = "sellerNick";
-	public static final String K_SID = "sid";
-	public static final String K_T = "t";
-	public static final String K_TIME = "time";
-	public static final String K_TOKEN = "token";
-	public static final String K_TOP_SESSION = "topSession";
-	public static final String K_TOP_TOKEN = "topToken";
-	public static final String K_TTID = "ttid";
-	public static final String K_USER_ID = "userId";
-	public static final String K_USERNAME = "username";
-	// other
-	public static final String CHARSET_NAME = "UTF-8";
-	public static final String SEPARATE_DOUBLE_COLON = "::";
 	// value
 	public static final String V_DISABLE = "disable";
 	public static final String V_ENABLE = "enable";
 	public static final String V_PROD = "prod";
-	public static final String V_SUCCESS = "success";
 	public static final String V_TEST = "test";
 }
